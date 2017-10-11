a31612f1e731d9b098aab9be3e814a1821a1350e
Daniels980 <31839458+Daniels980@users.noreply.github.com>
2017-10-11 13:46:06 +1100

    test commit 1
    the OG commit

---
+++ New Unity Project/Assets/Scripts/TestScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour 
+{
+
+	//this bit is here for both of us
+	int Both = 5;
+
+	//place int Tim here
+
+
+	//i'll put int Dan here
+
+
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
